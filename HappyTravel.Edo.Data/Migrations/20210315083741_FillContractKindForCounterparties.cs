aecb7428daf3a18c9af0ca5bdffe8d99d3d845d5
Hasmik <hasmik_harutyunyan@outlook.com>
2021-03-15 12:56:20 +0400

    added migration to fill ContractKindColumn in Counterparty table

---
+++ HappyTravel.Edo.Data/Migrations/20210315083741_FillContractKindForCounterparties.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FillContractKindForCounterparties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE  ""Counterparties"" 
+                            SET ""ContractKind"" = CASE WHEN ""PreferredPaymentMethod"" = 1 THEN 1 WHEN ""PreferredPaymentMethod"" = 2 THEN 3 END
+                            WHERE ""State"" = 1 AND ""ContractKind"" IS NULL;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
